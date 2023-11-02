25a4057219c98dd9d931fbd420291cadddcf9800
NithishDevapatla <nithishdevapatla336@gmail.com>
2023-11-02 16:05:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'; // Import React
+import './App.css';
+import Calci from './Components/Calci';
+
+function App() {
+  return (
+    <div>
+      <Calci /> {/* Render the Calci component */}
+    </div>
+  );
+}
+
+export default App;
